2da83bb85ae333d63201ff1bf1f324ee22079e99
zpneal <zpneal@msu.edu>
2022-05-03 16:39:00 -0400

    Add files via upload

--- R/incidence.from.congress.R
+++ R/incidence.from.congress.R
@@ -8,12 +8,36 @@
 #' @param weighted boolean: should sponsor-bill edges have a weight of 2, but cosponsor-bill edges have a weight of 1
 #' @param format string: format of output, one of c(\"data\", \"igraph\")
 #'
+#' @details
+#' The `incidence.from.congress()` function uses data from \href{https://www.congress.gov/}{https://www.congress.gov/} to
+#'    construct an incidence matrix or bipartite graph recording legislators' bill (co-)sponsorships. In an incidence matrix
+#'    **I**, entry *Iik = 1* if legislator *i* sponsored or co-sponsored bill *k*, and otherwise is 0. In a bipartite graph
+#'    **G**, a legislator *i* is connected to a bill *k* if *i* sponsored or co-sponsored *k*.
+#'
+#' In the US Congress, the law making process begins when a *sponsor* legislator introduces a bill in their chamber (House of
+#'    Representatives or Senate). Additional legislators in the same chamber can support the bill by joining as a *co-sponsor*.
+#'    The bill is discussed, revised, and possibly voted on in the chamber. If it passes in one chamber, it is sent to the other
+#'    chamber for further discussion, revision, and possibly a vote. If it passed both chambers, it is sent to the President. If
+#'    the President signs the bill, it becomes law.
+#'
+#' In the House of Representatives, legislators can introduce four types of bills: a House Bill (hb), a House Joint Resolution (hjres),
+#'    a House Concurrent Resolution (hconres), and a House Simple Resolution (hres). Similarly, in the Senate, legislators can introduce
+#'    four types of bills: a Senate Bill (s), a Senate Joint Resolution (sjres), a Senate Concurrent Resolution (sconres), and a Senate
+#'    Simple Resolution (sres). In both chambers, concurrent and simple resolutions are used for minor procedural matters and do not
+#'    have the force of law. Only bills and joint resolutions require the President's signature and have the force of law if signed.
+#'
 #' @return
 #' If `format = "data"`, a list containing an incidence matrix, a dataframe of legislator characteristics, and a dataframe of bill characteristics.
 #'
 #' If `format = "igraph"`, a bipartite igraph object composed of legislator vertices and bill vertices, each with vertex attributes.
 #'
 #' @export
+#'
+#' @examples
+#' \dontrun{
+#' D <- incidence.from.congress(session = 116, types = "s", format = "data")
+#' G <- incidence.from.congress(session = 115, types = c("hr", "hres"), format = "igraph")
+#' }
 incidence.from.congress <- function(session = NULL, types = NULL, weighted = FALSE, format = "data"){
 
   #Parameter check
@@ -24,7 +48,7 @@ incidence.from.congress <- function(session = NULL, types = NULL, weighted = FAL
   if (!(format %in% c("data", "igraph"))) {stop("format must be one of c(\"data\", \"igraph\")")}
 
   #Initialize data
-  dat <- data.frame(id = NULL, name = NULL, last = NULL, party = NULL, state = NULL, bill = NULL, title = NULL, area = NULL, status = NULL, weight = NULL)
+  dat <- data.frame(id = NULL, name = NULL, last = NULL, party = NULL, state = NULL, bill = NULL, introduced = NULL, title = NULL, area = NULL, status = NULL, weight = NULL)
 
   #Begin bill type loop
   for (type in types) {
@@ -44,6 +68,7 @@ incidence.from.congress <- function(session = NULL, types = NULL, weighted = FAL
 
       #Bill characteristics
       number <- paste0(xml2::xml_text(xml2::xml_find_first(bill, ".//billType")),xml2::xml_text(xml2::xml_find_first(bill, ".//billNumber")))
+      introduced <- xml2::xml_text(xml2::xml_find_first(bill, ".//introducedDate"))
       title <- xml2::xml_text(xml2::xml_find_first(bill, ".//title"))
       area <- xml2::xml_text(xml2::xml_find_first(bill, ".//policyArea"))
       status <- "Introduced"
@@ -71,8 +96,8 @@ incidence.from.congress <- function(session = NULL, types = NULL, weighted = FAL
       cs.state <- xml2::xml_text(xml2::xml_find_first(cosponsor, ".//state"))
 
       #Add to data
-      dat <- rbind(dat, data.frame(id = s.id, name = s.name, last = s.last, party = s.party, state = s.state, bill = number, title = title, area = area, status = status, weight = 2))
-      if (length(cs.id)>0) dat <- rbind(dat, data.frame(id = cs.id, name = cs.name, last = cs.last, party = cs.party, state = cs.state, bill = number, title = title, area = area, status = status, weight = 1))
+      dat <- rbind(dat, data.frame(id = s.id, name = s.name, last = s.last, party = s.party, state = s.state, bill = number, introduced = introduced, title = title, area = area, status = status, weight = 2))
+      if (length(cs.id)>0) dat <- rbind(dat, data.frame(id = cs.id, name = cs.name, last = cs.last, party = cs.party, state = cs.state, bill = number, introduced = introduced, title = title, area = area, status = status, weight = 1))
 
       utils::setTxtProgressBar(pb, file)
     } #End file loop
@@ -82,7 +107,7 @@ incidence.from.congress <- function(session = NULL, types = NULL, weighted = FAL
   #Prep sponsorship data and codebooks
   if (weighted) {sponsorship <- dat[c("name", "bill", "weight")]} else {sponsorship <- dat[c("name", "bill")]}
   legislator <- unique(dat[c("id", "name", "last", "party", "state")])
-  bills <- unique(dat[c("bill", "title", "area", "status")])
+  bills <- unique(dat[c("bill", "introduced", "title", "area", "status")])
 
   #Construct incidence matrix and codebooks
   if (format == "data") {
@@ -99,6 +124,7 @@ incidence.from.congress <- function(session = NULL, types = NULL, weighted = FAL
     igraph::V(G)[which(igraph::V(G)$type==F)]$state <- legislator$state
     igraph::V(G)[which(igraph::V(G)$type==F)]$last <- legislator$last
     igraph::V(G)[which(igraph::V(G)$type==F)]$id <- legislator$id
+    igraph::V(G)[which(igraph::V(G)$type==T)]$introduced <- bills$introduced
     igraph::V(G)[which(igraph::V(G)$type==T)]$title <- bills$title
     igraph::V(G)[which(igraph::V(G)$type==T)]$area <- bills$area
     igraph::V(G)[which(igraph::V(G)$type==T)]$status <- bills$status
